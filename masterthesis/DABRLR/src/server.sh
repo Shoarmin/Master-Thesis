ee79c861436b1a2b8ad3f9bcfe7f63deaac9a942
shoarmin <armins14@kpnmail.nl>
2023-01-23 13:03:19 +0100

    modified server

--- masterthesis/DABRLR/src/server.sh
+++ masterthesis/DABRLR/src/server.sh
@@ -18,7 +18,7 @@
 #SBATCH --cpus-per-task=1
 
 # The default memory per node is 1024 megabytes (1GB)
-#SBATCH --mem=12GB
+#SBATCH --mem=40GB
 
 #SBATCH --gres=gpu:a40:1
 
@@ -49,6 +49,6 @@ echo -ne "Running on node "
 hostname
 echo "Standard output:"
 
-srun python federated.py --data=fmnist --local_ep=3 --bs=256 --num_agents=10 --rounds=200 --num_corrupt=1 --poison_frac=0.5
+srun python federated.py --data=fedemnist --local_ep=3 --bs=256 --num_agents=10 --rounds=200 --num_corrupt=1 --poison_frac=0.5
 # Measure GPU usage of your job (result)
 /usr/bin/nvidia-smi --query-accounted-apps='gpu_utilization,mem_utilization,max_memory_usage,time' --format='csv' | /usr/bin/grep -v -F "$previous"
