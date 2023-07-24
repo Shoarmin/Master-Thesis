95d608b8996c8d0f18f4577025990fd3f71158eb
shoarmin <armins14@kpnmail.nl>
2023-07-24 20:05:03 +0200

    fix comed fmnist

--- masterthesis/DABRLR/src/experiments/fmnist_comed.sh
+++ masterthesis/DABRLR/src/experiments/fmnist_comed.sh
@@ -53,10 +53,10 @@ echo "Standard output:"
 #         srun python federated.py --data=fmnist --local_ep=2 --bs=256 --num_agents=10 --rounds=200 --num_corrupt=1 --poison_frac=0.5 --climg_attack=0 --pattern=sig --delta_val=$i --delta_attack=$i --aggr=comed --distribution=dirichlet --alpha=0.8 --attack_rounds=200
 # done
 
-srun python federated.py --data=fmnist --local_ep=2 --bs=256 --num_agents=10 --rounds=100 --num_corrupt=1 --poison_frac=0.5 --climg_attack=0 --pattern=sig --delta_val=30 --delta_attack=30 --aggr=comed --distribution=dirichlet --alpha=0.8 --attack_rounds=200
-srun python federated.py --data=fmnist --local_ep=2 --bs=256 --num_agents=10 --rounds=100 --num_corrupt=1 --poison_frac=0.5 --climg_attack=0 --pattern=sig --delta_val=30 --delta_attack=20 --aggr=comed --distribution=dirichlet --alpha=0.8 --attack_rounds=200
-srun python federated.py --data=fmnist --local_ep=2 --bs=256 --num_agents=10 --rounds=100 --num_corrupt=1 --poison_frac=0.5 --climg_attack=0 --pattern=sig --delta_val=60 --delta_attack=60 --aggr=comed --distribution=dirichlet --alpha=0.8 --attack_rounds=200
-srun python federated.py --data=fmnist --local_ep=2 --bs=256 --num_agents=10 --rounds=100 --num_corrupt=1 --poison_frac=0.5 --climg_attack=0 --pattern=sig --delta_val=60 --delta_attack=20 --aggr=comed --distribution=dirichlet --alpha=0.8 --attack_rounds=200
+srun python federated.py --data=fmnist --local_ep=2 --bs=256 --num_agents=10 --rounds=200 --num_corrupt=1 --poison_frac=0.5 --climg_attack=0 --pattern=sig --delta_val=40 --delta_attack=30 --aggr=comed --distribution=dirichlet --alpha=0.8 --attack_rounds=200
+srun python federated.py --data=fmnist --local_ep=2 --bs=256 --num_agents=10 --rounds=200 --num_corrupt=1 --poison_frac=0.5 --climg_attack=0 --pattern=sig --delta_val=40 --delta_attack=40 --aggr=comed --distribution=dirichlet --alpha=0.8 --attack_rounds=200
+srun python federated.py --data=fmnist --local_ep=2 --bs=256 --num_agents=10 --rounds=200 --num_corrupt=1 --poison_frac=0.5 --climg_attack=0 --pattern=sig --delta_val=60 --delta_attack=60 --aggr=comed --distribution=dirichlet --alpha=0.8 --attack_rounds=200
+srun python federated.py --data=fmnist --local_ep=2 --bs=256 --num_agents=10 --rounds=200 --num_corrupt=1 --poison_frac=0.5 --climg_attack=0 --pattern=sig --delta_val=60 --delta_attack=30 --aggr=comed --distribution=dirichlet --alpha=0.8 --attack_rounds=200
 
 # Measure GPU usage of your job (result)
 /usr/bin/nvidia-smi --query-accounted-apps='gpu_utilization,mem_utilization,max_memory_usage,time' --format='csv' | /usr/bin/grep -v -F "$previous"
