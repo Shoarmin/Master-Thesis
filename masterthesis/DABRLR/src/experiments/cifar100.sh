66e24ec2543f1725cb4df9fd7719172f95a8b266
shoarmin <armins14@kpnmail.nl>
2023-07-25 11:27:48 +0200

    fix cifar100

--- masterthesis/DABRLR/src/experiments/cifar100.sh
+++ masterthesis/DABRLR/src/experiments/cifar100.sh
@@ -49,8 +49,8 @@ echo -ne "Running on node "
 hostname
 echo "Standard output:"
 
-for ((i = 5; i <= 20; i += 5)); do
-        srun python federated.py --data=cifar100 --local_ep=2 --bs=256 --num_agents=10 --rounds=60 --num_corrupt=1 --poison_frac=0.5 --climg_attack=0 --pattern=sig --delta_val=20 --delta_attack=$i
+for ((i = 10; i <= 30; i += 5)); do
+        srun python federated.py --data=cifar100 --local_ep=2 --bs=256 --num_agents=10 --rounds=100 --num_corrupt=1 --poison_frac=0.5 --climg_attack=0 --pattern=sig --delta_val=30 --delta_attack=$i
 done
 
 # Measure GPU usage of your job (result)
